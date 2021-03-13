fce6211a9f0937d81e6858d79b7a7f139ef8f25b
gustavomerini <gutomerini@gmail.com>
2021-03-13 09:51:27 -0300

    FEAT: Lib and showcase init

---
+++ projects/lib/src/calendar/calendar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'calendar-lib',
+  templateUrl: './calendar.component.html',
+  styles: ['./calendar.component.scss']
+})
+export class CalendarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/lib/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of lib
+ */
+
+export * from './calendar/calendar.service';
+export * from './calendar/calendar.component';
+export * from './calendar/calendar.module';
