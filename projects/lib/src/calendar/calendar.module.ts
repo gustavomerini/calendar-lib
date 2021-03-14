7700fbc20b8a06180170ab94648db0f5dd49bd77
gustavomerini <gutomerini@gmail.com>
2021-03-14 00:06:35 -0300

    FEAT: Add Angular Material

--- projects/lib/src/calendar/calendar.module.ts
+++ projects/lib/src/calendar/calendar.module.ts
@@ -4,11 +4,12 @@ import { CalendarComponent } from './calendar.component';
 import { CalendarDayComponent } from './components/calendar-day/calendar-day.component';
 import { ReminderComponent } from './components/reminder/reminder.component';
 import { CalendarService } from './services/calendar.service';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [CalendarComponent, CalendarDayComponent, ReminderComponent],
   providers: [CalendarService],
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule],
   exports: [CalendarComponent]
 })
 export class CalendarModule { }
